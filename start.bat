c55c5eb53ed6fe8bb173225c76232013a3fc9dea
晚风拂柳颜 <434857005@qq.com>
2024-05-13 01:20:45 +0800

    启动脚本调整

---
+++ start.bat
@@ -0,0 +1,5 @@
+start main.exe
+start http://127.0.0.1:5708/active
+REM sleep 5
+REM content = $(curl http://127.0.0.1:5708/active | tee startlog.json)
+REM echo "$content"
--- start.sh
+++
@@ -1,4 +0,0 @@
-start main.exe
-sleep 5
-content = $(curl http://127.0.0.1:5708/active | tee startlog.json)
-echo "$content"
